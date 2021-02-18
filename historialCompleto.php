28e98afa6d71214ab44e21649b292b7b2abdc20d
brix <brix@nice.com>
2021-02-18 14:26:31 -0600

    tablas responsive con encabezado fijo

--- historialCompleto.php
+++ historialCompleto.php
@@ -14,76 +14,82 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
   <link rel="shortcut icon" href="CSS/IMG/image001.ico">
   <link rel="stylesheet" href="CSS/bootstrap.css">
   <title>Historial Completo</title>
+  <style type="text/css">
+    tr th {
+      position: sticky;
+      top: 0;
+      z-index: 10;
+      background-color: #ffffff;
+    }
+
+    .table-responsive {
+      height: 436px;
+      overflow: scroll;
+    }
+  </style>
 </head>
 
 <body>
   <div>
-    <nav class="navbar sticky-top navbar-light" style="background-color: #e3f2fd;" justify-content-between">
+    <nav class="navbar sticky-top navbar-light justify-content-between" style="background-color: #e3f2fd;">
       <a class="btn btn-danger" href="menuPrincipal.php">Atras</a>
-      <form class="form-inline">
-        <input class="form-control mr-sm-2" id="search" type="search" placeholder="Buscar" aria-label="Search">
+      <button type="submit" id="export_data" name='export_data' value="Export to excel" class="btn btn-info">Export to excel</button>
+      <form class="form-inline" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+        <input class="form-control mr-sm-1" id="search" type="search" placeholder="Buscar" aria-label="Search">
       </form>
     </nav>
     <img src="CSS/IMG/image001.png" class="img-fluid" alt="Responsive image">
   </div>
-  <div class="table-responsive">
-    <table id="mytable" class="table table-hover">
-      <thead class="thead-dark">
-        <tr>
-          <th class="header" scope="col">ID</th>
-          <th class="header" scope="col">Numero de Contenedor</th>
-          <th class="header" scope="col">Chasis</th>
-          <th class="header" scope="col">Genset</th>
-          <th class="header" scope="col">Placa Chasis</th>
-          <th class="header" scope="col">Fecha Ingreso</th>
-          <th class="header" scope="col">Piloto de Ingreso</th>
-          <th class="header" scope="col">Placa de Piloto de Ingreso</th>
-          <th class="header" scope="col">Empresa de Ingreso</th>
-          <th class="header" scope="col">Fecha de Salida</th>
-          <th class="header" scope="col">Booking</th>
-          <th class="header" scope="col">Piloto de Salida</th>
-          <th class="header" scope="col">Placa de Piloto de Salida</th>
-          <th class="header" scope="col">Empresa de Salida</th>
-          <th class="header" scope="col">Dias</th>
-          <th class="header" scope="col">Tamaño</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $sel = $con->query("SELECT * FROM contenedores ");
-        while ($fila = $sel->fetch_assoc()) {
-        ?>
-          <tr>
-            <td scope="row"><?php echo $fila['id'] ?></td>
-            <td scope="row"><?php echo $fila['num_contenedor'] ?></td>
-            <td scope="row"><?php echo $fila['chasis'] ?></td>
-            <td scope="row"><?php echo $fila['genset'] ?></td>
-            <td scope="row"><?php echo $fila['placa_chasis'] ?></td>
-            <td scope="row"><?php echo $fila['fecha_ingreso'] ?></td>
-            <td scope="row"><?php echo $fila['piloto_ingreso'] ?></td>
-            <td scope="row"><?php echo $fila['placa_piloto_ingreso'] ?></td>
-            <td scope="row"><?php echo $fila['empresa_ingreso'] ?></td>
-            <td scope="row"><?php echo $fila['fecha_salida'] ?></td>
-            <td scope="row"><?php echo $fila['booking'] ?></td>
-            <td scope="row"><?php echo $fila['piloto_salida'] ?></td>
-            <td scope="row"><?php echo $fila['placa_piloto_salida'] ?></td>
-            <td scope="row"><?php echo $fila['empresa_salida'] ?></td>
-            <td scope="row"><?php echo $fila['dias'] ?></td>
-            <td scope="row"><?php echo $fila['tamano'] ?></td>
-          </tr>
-        <?php } ?>
-      </tbody>
+  <div class="table-responsive"">
+    <table id=" mytable" class="table table-bordered table-hover table-sm table-condensed">
+    <tr>
+      <th class="bg-light" scope="col">ID</th>
+      <th class="bg-light" scope="col">Numero de Contenedor</th>
+      <th class="bg-light" scope="col">Chasis</th>
+      <th class="bg-light" scope="col">Genset</th>
+      <th class="bg-light" scope="col">Fecha Ingreso</th>
+      <th class="bg-light" scope="col">Piloto de Ingreso</th>
+      <th class="bg-light" scope="col">Placa de Piloto de Ingreso</th>
+      <th class="bg-light" scope="col">Empresa de Ingreso</th>
+      <th class="bg-light" scope="col">Fecha de Salida</th>
+      <th class="bg-light" scope="col">Booking de Salida</th>
+      <th class="bg-light" scope="col">Piloto de Salida</th>
+      <th class="bg-light" scope="col">Placa de Piloto de Salida</th>
+      <th class="bg-light" scope="col">Empresa de Salida</th>
+      <th class="bg-light" scope="col">Dias</th>
+      <th class="bg-light" scope="col">Tamaño</th>
+    </tr>
+    <?php
+    $sel = $con->query("SELECT * FROM contenedores ");
+    while ($fila = $sel->fetch_assoc()) {
+    ?>
+      <tr>
+        <td scope="row"><?php echo $fila['id'] ?></td>
+        <td scope="row"><?php echo $fila['num_contenedor'] ?></td>
+        <td scope="row"><?php echo $fila['chasis'] ?></td>
+        <td scope="row"><?php echo $fila['genset'] ?></td>
+        <td scope="row"><?php echo $fila['fecha_ingreso'] ?></td>
+        <td scope="row"><?php echo $fila['piloto_ingreso'] ?></td>
+        <td scope="row"><?php echo $fila['placa_piloto_ingreso'] ?></td>
+        <td scope="row"><?php echo $fila['empresa_ingreso'] ?></td>
+        <td scope="row"><?php echo $fila['fecha_salida'] ?></td>
+        <td scope="row"><?php echo $fila['booking'] ?></td>
+        <td scope="row"><?php echo $fila['piloto_salida'] ?></td>
+        <td scope="row"><?php echo $fila['placa_piloto_salida'] ?></td>
+        <td scope="row"><?php echo $fila['empresa_salida'] ?></td>
+        <td scope="row"><?php echo $fila['dias'] ?></td>
+        <td scope="row"><?php echo $fila['tamano'] ?></td>
+      </tr>
+    <?php } ?>
     </table>
   </div>
-  
+
   <script src='js/jquery.min.js'></script>
   <script src="JS/bootstrap.js"></script>
   <script>
-    // Write on keyup event of keyword input element
     $(document).ready(function() {
       $("#search").keyup(function() {
         _this = this;
-        // Show only matching TR, hide rest of them
         $.each($("#mytable tbody tr"), function() {
           if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
             $(this).hide();
--- inventarioMes.php
+++ inventarioMes.php
@@ -14,47 +14,65 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
   <link rel="stylesheet" href="CSS/bootstrap.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Inventario</title>
+  <style type="text/css">
+    tr th {
+      position: sticky;
+      top: 0;
+      z-index: 10;
+      background-color: #ffffff;
+    }
+
+    .table-responsive {
+      height: 436px;
+      overflow: scroll;
+    }
+  </style>
 </head>
-<header>
-<nav class="navbar navbar-light" style="background-color: #e3f2fd;" justify-content-between navbar-static-top">
+
+<body>
+  <header>
+    <nav class="navbar navbar-light justify-content-between navbar-static-top" style="background-color: #e3f2fd;">
       <a class="btn btn-danger" href="menuPrincipal.php">Atras</a>
+      <button type="submit" id="export_data" name='export_data' value="Export to excel" class="btn btn-info">Export to excel</button>
       <form class="form-inline">
         <input class="form-control mr-sm-2" id="search" type="search" placeholder="Search" aria-label="Search">
       </form>
     </nav>
-</header>
-
-<body>
-<img src="CSS/IMG/image001.png" class="img-fluid" alt="Responsive image">
-  <table id="mytable" class="table table-bordered">
-    <th scope="col thead-dark">ID</th>
-    <th scope="col">Numero de Contenedor</th>
-    <th scope="col">Chasis</th>
-    <th scope="col">Genset</th>
-    <th scope="col">Placa Chasis</th>
-    <th scope="col">Fecha Ingreso</th>
-    <th scope="col">Hora Ingreso</th>
-    <th scope="col">Tamaño</th>
-    <th scope="col">Ejes</th>
-    <th scope="col">Observacion</th>
-    <?php
-    $sel = $con->query("SELECT * FROM contenedores WHERE estado='Activo'");
-    while ($fila = $sel->fetch_assoc()) {
-    ?>
+  </header>
+  <img src="CSS/IMG/image001.png" class="img-fluid" alt="Responsive image">
+  <div class="table-responsive">
+    <table id="mytable" class="table table-bordered table-hover table-sm table-condensed">
       <tr>
-        <td scope="row"><?php echo $fila['id'] ?></td>
-        <td scope="row"><a href="gate-out.php?id=<?php echo $fila['id'] ?>"><?php echo $fila['num_contenedor'] ?></a></td>
-        <td scope="row"><?php echo $fila['chasis'] ?></td>
-        <td scope="row"><?php echo $fila['genset'] ?></td>
-        <td scope="row"><?php echo $fila['placa_chasis'] ?></td>
-        <td scope="row"><?php echo $fila['fecha_ingreso'] ?></td>
-        <td scope="row"><?php echo $fila['hora_ingreso'] ?></td>
-        <td scope="row"><?php echo $fila['tamano'] ?></td>
-        <td scope="row"><?php echo $fila['ejes'] ?></td>
-        <td scope="row"><a href="gate-out.php?id=<?php echo $fila['id'] ?>"><?php echo $fila['observacion'] ?></a></td>
+        <th class="bg-light" scope="col">ID</th>
+        <th class="bg-light" scope="col">Numero de Contenedor</th>
+        <th class="bg-light" scope="col">Chasis</th>
+        <th class="bg-light" scope="col">Genset</th>
+        <th class="bg-light" scope="col">Placa Chasis</th>
+        <th class="bg-light" scope="col">Fecha Ingreso</th>
+        <th class="bg-light" scope="col">Hora Ingreso</th>
+        <th class="bg-light" scope="col">Tamaño</th>
+        <th class="bg-light" scope="col">Ejes</th>
+        <th class="bg-light" scope="col">Observacion</th>
       </tr>
-    <?php } ?>
-  </table>
+      <?php
+      $sel = $con->query("SELECT * FROM contenedores WHERE estado='Activo'");
+      while ($fila = $sel->fetch_assoc()) {
+      ?>
+        <tr>
+          <td scope="row"><?php echo $fila['id'] ?></td>
+          <td scope="row"><a href="gate-out.php?id=<?php echo $fila['id'] ?>"><?php echo $fila['num_contenedor'] ?></a></td>
+          <td scope="row"><?php echo $fila['chasis'] ?></td>
+          <td scope="row"><?php echo $fila['genset'] ?></td>
+          <td scope="row"><?php echo $fila['placa_chasis'] ?></td>
+          <td scope="row"><?php echo $fila['fecha_ingreso'] ?></td>
+          <td scope="row"><?php echo $fila['hora_ingreso'] ?></td>
+          <td scope="row"><?php echo $fila['tamano'] ?></td>
+          <td scope="row"><?php echo $fila['ejes'] ?></td>
+          <td scope="row"><a href="gate-out.php?id=<?php echo $fila['id'] ?>"><?php echo $fila['observacion'] ?></a></td>
+        </tr>
+      <?php } ?>
+    </table>
+  </div>
   <script src='js/jquery.min.js'></script>
   <script src="JS/bootstrap.js"></script>
   <script>
