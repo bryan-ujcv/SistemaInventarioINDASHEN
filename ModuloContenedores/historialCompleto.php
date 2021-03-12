08eef17ec0bbe514d826a3e1f1561e8601a57845
brix <brix@nice.com>
2021-03-12 13:59:16 -0600

    se elimino script de la barra de busqueda

--- ModuloContenedores/historialCompleto.php
+++ ModuloContenedores/historialCompleto.php
@@ -237,19 +237,6 @@ if (isset($_POST["export"])) {
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script>
-  $(document).ready(function() {
-    $("#search").keyup(function() {
-      _this = this;
-      $.each($("#mytable tbody tr"), function() {
-        if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
-          $(this).hide();
-        else
-          $(this).show();
-      });
-    });
-  });
-</script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.23/datatables.min.css" />
 <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.23/datatables.min.js"></script>
 <script src="../JS/table.js"></script>
